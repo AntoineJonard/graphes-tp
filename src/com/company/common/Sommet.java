5ca2cc7855671f0bb3e64e109eb44a11a3a663cc
AntoineJonard <antoine.jonard@orange.fr>
2021-05-12 14:02:20 +0200

    feat(liste adjacence + sommet) : id généré sommet + fct de base liste adjacnce

--- src/com/company/common/Sommet.java
+++ src/com/company/common/Sommet.java
@@ -1,12 +1,15 @@
 package com.company.common;
 
+import java.util.Objects;
+
 public class Sommet {
 
     private int id;
     private String name;
+    private static int CPT_ID = 0;
 
-    public Sommet(int id, String name) {
-        this.id = id;
+    public Sommet(String name) {
+        this.id = CPT_ID++;
         this.name = name;
     }
 
@@ -25,4 +28,13 @@ public class Sommet {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sommet sommet = (Sommet) o;
+        return id == sommet.id && Objects.equals(name, sommet.name);
+    }
+
 }
--- src/com/company/liste/ListeAdjacence.java
+++ src/com/company/liste/ListeAdjacence.java
@@ -1,4 +1,49 @@
 package com.company.liste;
 
+import com.company.common.Sommet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ListeAdjacence {
+
+    private final HashMap<Sommet, List<Sommet>> listeAdjacence;
+
+    public ListeAdjacence() {
+        this.listeAdjacence = new HashMap<>();
+    }
+
+    public void addSommet(Sommet s){
+        this.listeAdjacence.put(s,new ArrayList<>());
+    }
+
+    public void addArrete(Sommet s1, Sommet s2){
+        this.listeAdjacence.get(s1).add(s2);
+        this.listeAdjacence.get(s2).add(s1);
+    }
+
+    public void removeArrete(Sommet s1, Sommet s2){
+        this.listeAdjacence.get(s1).remove(s2);
+        this.listeAdjacence.get(s2).remove(s1);
+    }
+
+    public void removeSommet(Sommet s){
+        this.listeAdjacence.remove(s);
+        for (Map.Entry entry : this.listeAdjacence.entrySet()){
+            ArrayList<Sommet> sommets = ((ArrayList<Sommet>)entry.getValue());
+            for (int i = 0 ; i < sommets.size() ; i++){
+                if (sommets.get(i).equals(s)){
+                    sommets.remove(sommets.get(i));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ListeAdjacence listeAdjacence = new ListeAdjacence();
+
+        listeAdjacence.addSommet(new Sommet("1"));
+    }
 }
