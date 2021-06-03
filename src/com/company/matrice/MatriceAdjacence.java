d8b7bc90eabcc96f9c0183a4a65109877c0e8104
Paul Hueso <paul.hueso@gmail.com>
2021-06-03 09:46:47 +0200

    feat(MatriceAdjacence): ajout est_Stable_De, est_Clique_De, est_Sous_Graphe_De, est_Partiel_De

--- src/com/company/matrice/MatriceAdjacence.java
+++ src/com/company/matrice/MatriceAdjacence.java
@@ -12,7 +12,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MatriceAdjacence {
+    /** Matrice représentant les arretes */
     List<ArrayList<Integer>> matrice;
+    /** Liste des sommets liés à leur Identifiant */
     ArrayList<Sommet> sommetsList;
 
     public MatriceAdjacence(){
@@ -52,12 +54,12 @@ public class MatriceAdjacence {
     public void add_sommet(Sommet s){
         if(matrice.isEmpty()){
             sommetsList.add(s);
-            matrice.add(new ArrayList<Integer>());
+            matrice.add(new ArrayList<>());
             matrice.get(0).add(0);
         } else if(!sommetsList.contains(s)){
             sommetsList.add(s);
             matrice.forEach(l -> l.add(0));
-            matrice.add(new ArrayList<Integer>());
+            matrice.add(new ArrayList<>());
             for(int i = 0; i < matrice.get(0).size(); i++){
                 matrice.get(matrice.size()-1).add(0);
             }
@@ -124,25 +126,35 @@ public class MatriceAdjacence {
     }
 
     /** Renvoie true si les sommets du graphe actuel sont inclus dans le graphe passe en parametres, faux sinon*/
-    public boolean inclus_sommet(MatriceAdjacence G, boolean strict){
-        /** On recupere les noms des sommets actuels*/
+    public boolean sommets_Inclus_Dans(MatriceAdjacence G, boolean strict){
+        /* On recupere les noms des sommets actuels*/
         List<String> nomSommets = sommetsList.stream().map(Sommet::getName).collect(Collectors.toList());
-        /** On recupere les noms des sommets du graphe passes en parametres*/
+        /* On recupere les noms des sommets du graphe passes en parametres*/
         List<String> nomSommetsAutre = G.sommetsList.stream().map(Sommet::getName).collect(Collectors.toList());
 
-        /** Si strict alors renvoie true si G inclus dans G' et G' pas inclus dans G*/
+        /*Si strict alors renvoie true si G inclus dans G' et G' pas inclus dans G*/
         if (strict) return nomSommetsAutre.containsAll(nomSommets) && !nomSommets.containsAll(nomSommetsAutre);
-        /** Renvoie true si G inclus G', peut renvoyer true si G = G'*/
+        /* Renvoie true si G inclus G', peut renvoyer true si G = G'*/
         return nomSommetsAutre.containsAll(nomSommets);
+    }
+
+    /** Compte le nombre d'arretes du graphe */
+    public int nombre_Arretes(){
+        int nbArcs = 0;
+        for(int i = 1; i < matrice.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(matrice.get(i).get(j) == 1) nbArcs++;
+            }
+        }
+        return nbArcs;
     }
 
     /** On verifie si le premier graphe est inclus dans G, ensuite on compte les arretes de G et
      * on verifie si nbArcs < nbArcsG
      * */
-    public boolean inclus_aretes(MatriceAdjacence G){
+    public boolean arretes_Incluses_Dans(MatriceAdjacence G2){
         int nbArcs = 0;
         int nbArcsG = 0;
-
 
         //Recuperer les deux noms pour chaque arrete
         for(int i = 1; i < matrice.size(); i++){
@@ -155,25 +167,45 @@ public class MatriceAdjacence {
                     String name2 = sommetsList.get(j).getName();
 
                     //Trouver les id des sommmets qui ont ces noms dans le graphe G
-                    for (int k = 0; k < G.sommetsList.size(); k++) {
-                        if(G.sommetsList.get(k).getName().equals(name1)) {id1 = k;}
-                        else if(G.sommetsList.get(k).getName().equals(name2)) {id2 = k;}
+                    for (int k = 0; k < G2.sommetsList.size(); k++) {
+                        if(G2.sommetsList.get(k).getName().equals(name1)) {id1 = k;}
+                        else if(G2.sommetsList.get(k).getName().equals(name2)) {id2 = k;}
                     }
                     if(id1 == -1 || id2 == -1) return false;
 
                     //On s'arrete si l'arrete n'est pas presente
-                    if(G.matrice.get(id1).get(id2) != 1 || G.matrice.get(id2).get(id1) != 1 ) {  return false;}
+                    if(G2.matrice.get(id1).get(id2) != 1 || G2.matrice.get(id2).get(id1) != 1 ) {  return false;}
                 }
             }
         }
 
         //On verifie si nbArcs < NbArcsG
-        for(int i = 1; i < G.matrice.size(); i++){
-            for(int j = 0; j < i; j++){
-                if(G.matrice.get(i).get(j) == 1) nbArcsG++;
-            }
-        }
+        nbArcsG = G2.nombre_Arretes();
         return nbArcs < nbArcsG;
+    }
+    /** Sommet strictement inclus, arretes incluses */
+    public boolean est_Partiel_De(MatriceAdjacence G2){
+        return this.arretes_Incluses_Dans(G2) && sommets_Inclus_Dans(G2, false);
+    }
+    /** Arretes et sommets strictement inclus */
+    public boolean est_Sous_Graphe_De(MatriceAdjacence G2){
+        return this.arretes_Incluses_Dans(G2) && this.sommets_Inclus_Dans(G2, true);
+    }
+
+    /** */
+    /*public est_Sous_Graphe_Partiel(MatriceAdjacence G2){
+
+    }*/
+
+    /** On verifie que G2 est bien un sous graphe de G, et que G2 est complet*/
+    public boolean est_Clique_De(MatriceAdjacence G2){
+        int nbSommets = G2.sommetsList.size();
+        return (nbSommets*(nbSommets-1))/2 == G2.nombre_Arretes() && G2.est_Sous_Graphe_De(this);
+    }
+
+    /** On verifie que G2 est bien un sous graphe de G, et que G2 n'a pas d'arretes */
+    public boolean est_Stable_De(MatriceAdjacence G2){
+        return G2.nombre_Arretes() == 0 && G2.est_Sous_Graphe_De(this);
     }
 
     public void afficher() {
@@ -188,7 +220,7 @@ public class MatriceAdjacence {
     public static void main(String[] args) throws IOException {
         MatriceAdjacence m = new MatriceAdjacence("graphe.txt");
         MatriceAdjacence G = new MatriceAdjacence("graphe2.txt");
-        System.out.println(m.inclus_aretes(G));
+        System.out.println(m.arretes_Incluses_Dans(G));
         //m.afficher();
 
 
